e9df55cb1095838efa4f497a5e27c4874aa6c797
Ashutosh <ashunandanwar98@gmail.com>
2024-07-15 23:24:16 +0530

    Narrative Story Added using Timeline

--- Assets/Breakable.cs
+++ Assets/Breakable.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Breakable : MonoBehaviour
 {
     public List<GameObject> breakableItems;
     private float timeToBreak = 2f;
     private float currentTime = 0;
+    public UnityEvent OnBreak;
     void Start()
     {
         foreach(var item in breakableItems)
@@ -33,6 +35,8 @@ public class Breakable : MonoBehaviour
                 item.SetActive(true);
                 item.transform.parent = null;
             }
+            OnBreak.Invoke();
+
             gameObject.SetActive(false);
 
         }        
