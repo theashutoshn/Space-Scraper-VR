6c1512050130032ca91d16a54b39cd3cf07ba69b
Ashutosh <ashunandanwar98@gmail.com>
2024-07-15 21:16:15 +0530

    Climb Interactable & Wheel Lever

---
+++ Assets/SpaceOutside.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils.GUI;
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+
+public class SpaceOutside : MonoBehaviour
+{
+    public XRLever lever;
+    public XRKnob knob;
+
+    public float forwardSpeed;
+    public float sideSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardVelocity = forwardSpeed * (lever.value ? 0 : 1); // (lever.value ? 1 : 0) I swapped the value based on my lever position on my model
+        float sideVelocity = sideSpeed * (lever.value ? 0 : 1) * Mathf.Lerp(-1, 1, knob.value);
+
+        Vector3 velocity = new Vector3(sideVelocity, 0, forwardVelocity);
+        transform.position += velocity * Time.deltaTime;
+    }
+}
